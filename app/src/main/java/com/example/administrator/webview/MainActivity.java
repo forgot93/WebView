43597e5024612da279e7a4fa6ee14160b87f8965
forgot93 <771351522@qq.com>
2016-07-15 16:55:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/webview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.administrator.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.wv_webview);
+        loadWeb();
+    }
+
+    public void loadWeb(){
+        String url = "https://www.baidu.com/";
+        //此方法可以在webview中打开链接而不会跳转到外部浏览器
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //重写onKeyDown，当浏览网页，WebView可以后退时执行后退操作。
+        if(keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
